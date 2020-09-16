c18c30dfaaca638048b76ab0816e791c94fabe36
Red <hongqiang102@qq.com>
2020-09-16 23:28:04 +0800

    lesson-16 eureka服务注册中心

--- provider-payment8001/src/main/java/com/blossom/springcloud/controller/PaymentController.java
+++ provider-payment8001/src/main/java/com/blossom/springcloud/controller/PaymentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
+@SuppressWarnings("unchecked")
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
