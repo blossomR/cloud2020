07cf6b67d5c540602ee7b3077a627a29726323bb
Red <hongqiang102@qq.com>
2020-09-18 23:29:27 +0800

    lesson-17 注册服务中心8001

--- provider-payment8001/src/main/java/com/blossom/springcloud/PaymentMain8001.java
+++ provider-payment8001/src/main/java/com/blossom/springcloud/PaymentMain8001.java
@@ -3,9 +3,11 @@ package com.blossom.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.blossom.springcloud.mapper")
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
