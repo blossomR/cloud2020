3a71d8bc66c39c22d399b5e7f20d81edfd006145
Red <hongqiang102@qq.com>
2020-11-08 13:49:27 +0800

    lesson 30

--- consumer-zk-order80/src/main/java/com/blossom/springcloud/OrderZkMain80.java
+++ consumer-zk-order80/src/main/java/com/blossom/springcloud/OrderZkMain80.java
@@ -3,10 +3,10 @@ package com.blossom.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class OrderZkMain80 {
 
     public static void main(String[] args) {
